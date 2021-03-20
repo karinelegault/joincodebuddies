64cba9a3a08d9fe31d23b76b546fb300ad8dc5d6
gad dayan <gadou86@gmail.com>
2021-03-20 15:32:54 -0400

    Added projects ALL controllers and view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,29 +8,29 @@ class ProjectsController < ApplicationController
         @project = Project.find(params[:id])
     end
 
-    # def new
-    #     @project = Project.new
-    # end
+    def new
+        @project = Project.new
+    end
 
-    # def create
-    #     @project = Project.new(project_params)
-    #     @project.user = current_user
-    #     if @project.save
-    #         redirect_to project_path(@project)
-    #     else
-    #         render :new
-    #     end
-    # end
+    def create
+        @project = Project.new(project_params)
+        @project.user = current_user
+        if @project.save
+            redirect_to project_path(@project)
+        else
+            render :new
+        end
+    end
 
-    # def edit
-    #     @project = Project.find(params[:id])
-    # end
+    def edit
+        @project = Project.find(params[:id])
+    end
 
-    # def update
-    #     @project = Project.find(params[:id])
-    #     @project.update(project_params)
-    #     redirect_to project_path(@project)
-    # end
+    def update
+        @project = Project.find(params[:id])
+        @project.update(project_params)
+        redirect_to project_path(@project)
+    end
 
     private
 
