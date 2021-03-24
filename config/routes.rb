bd368014e479206f5d7d0e685c53f8f05e874c59
gad dayan <gadou86@gmail.com>
2021-03-23 21:20:02 -0400

    added profile pages for all users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "projects#index"
   resources :projects, only: [:create, :edit, :show, :update, :new]
+  get "profiles", to: "profiles#index", as: :profiles_index
 end
