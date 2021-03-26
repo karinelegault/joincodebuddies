3db5de9f7c222a953c2ffe197d9bf999b6c873c6
gad dayan <gadou86@gmail.com>
2021-03-26 00:33:39 -0400

    Added city for seed and map is functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def index
         @users = User.all
 
-        @markers = @users.geocoded.map do |user|
+        @markers = @users.map do |user|
             {
               lat: user.latitude,
               lng: user.longitude
