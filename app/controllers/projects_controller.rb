4e5e41bf3234b36ee6ac5af2142f77af2d609916
gad dayan <gadou86@gmail.com>
2021-04-15 20:12:03 -0400

    Fix css of the chatroom and added pop i will style it in the next branch

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
     def show
       @user = current_user
       @project = Project.find(params[:id])
+      @chatroom = @project.chatroom
     end
 
     def new
