738cdc021c74cee9c4500f999df49cd5553d5efc
Karine Legault <karine.legault.mallette14@gmail.com>
2021-04-15 20:14:56 -0400

    metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def home
-      @user = current_user
-    end
+  def home
+    @user = current_user
+  end
+
+  def default_url_options
+    { host: ENV["joincodebuddies.com"] || "localhost:3000" }
+  end
 end
