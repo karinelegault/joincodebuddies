2f8e2a77f5878d69e981e7e2a16fe45ed974e910
Mirchev Pascal <pascalmirchev2000@gmail.com>
2021-04-17 11:41:12 -0400

    Here's my projects

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,73 +1,88 @@
 class RequestsController < ApplicationController
 
-    def join_request
-        @user = current_user
-        @project = Project.find(params[:project_id])
-        @request = Request.new
-        @request.project_id = params[:project_id]
-        @request.user_id = current_user.id
-        @request.status = "pending"
-        if request_exist? == true
-          # raise
-          flash.now[:notice] = "You've already sent a request"
-          render "projects/show"
-        else
-          @request.save!
+  def join_request
+    @user = current_user
+    @project = Project.find(params[:project_id])
+    @request = Request.new
+    @request.project_id = params[:project_id]
+    @request.user_id = current_user.id
+    @request.status = "pending"
+    if request_exist? == true
+      # raise
+      flash.now[:notice] = "You've already sent a request"
+      render "projects/show"
+    else
+      @request.save!
+    end
+    render "projects/confirmation"
+  end
+
+  def outgoing_requests
+    @requests = Request.where(user_id: current_user.id)
+  end
+
+  def incoming_requests
+    projects = Project.where(user_id: current_user.id)
+    my_requests = projects.map { |project| project.requests }
+    @requests = my_requests.flatten
+  end
+
+  def accept_requests
+    @request = Request.find(params[:id])
+    @request.status = "accepted"
+    @project = Project.find(@request.project_id)
+    @user = User.find(@request.user_id)
+    @project.teammates << @user.id
+    @project.save
+    @request.save
+    redirect_to user_dashboard_requests_path
+  end
+
+  def deny_requests
+    @request = Request.find(params[:id])
+    @request.status = "denied"
+    @request.save
+    redirect_to user_dashboard_requests_path
+  end
+
+  def components
+    @user = current_user
+  end
+
+  def request_exist?
+    array = Request.where(project_id: @project.id, user_id: current_user.id)
+    if array.length > 0
+      return true
+    end
+  end
+
+  def user_dashboard_requests
+    @user = current_user
+    projects = Project.where(user_id: current_user.id)
+    my_requests = projects.map { |project| project.requests }
+    @incoming_requests = my_requests.flatten
+    @outgoing_requests = Request.where(user_id: current_user.id)
+    @my_projects = find_my_projects
+    raise
+  end
+
+  def find_my_projects
+    my_projects = []
+    Project.all.each do | project|
+      if project.teammates != nil
+        if project.teammates.include? current_user.id
+          my_projects << project
         end
-        render "projects/confirmation"
-    end
-
-    def outgoing_requests
-       @requests = Request.where(user_id: current_user.id)
-    end
-
-    def incoming_requests
-      projects = Project.where(user_id: current_user.id)
-      my_requests = projects.map { |project| project.requests }
-      @requests = my_requests.flatten
-    end
-
-    def accept_requests
-        @request = Request.find(params[:id])
-        @request.status = "accepted"
-        @project = Project.find(@request.project_id)
-        @user = User.find(@request.user_id)
-        @project.teammates << @user.id
-        @project.save
-        @request.save
-        redirect_to user_dashboard_requests_path
-    end
-
-    def deny_requests
-        @request = Request.find(params[:id])
-        @request.status = "denied"
-        @request.save
-        redirect_to user_dashboard_requests_path
-    end
-
-    def components
-      @user = current_user
-    end
-
-    def request_exist?
-      array = Request.where(project_id: @project.id, user_id: current_user.id)
-      if array.length > 0
-        return true
       end
     end
+    my_projects << Project.where(user_id: current_user.id)
+    return my_projects.flatten
+  end
 
-    def user_dashboard_requests
-      @user = current_user
-      projects = Project.where(user_id: current_user.id)
-      my_requests = projects.map { |project| project.requests }
-      @incoming_requests = my_requests.flatten
-      @outgoing_requests = Request.where(user_id: current_user.id)
+  private
 
-    end
+  def request_params
+    params.require(:request).permit(:project_id, :user_id, :status)
+  end
 
-    private
-
-    def request_params
-        params.require(:request).permit(:project_id, :user_id, :status)
-    end
 end
