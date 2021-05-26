f155a4c99f4ea115645a038208392bbb4bf70de8
Mirchev Pascal <pascalmirchev2000@gmail.com>
2021-05-26 16:59:55 -0400

    Forcing all redirect through SSL for https redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  
   def home
     @user = current_user
   end
-
+  
   def default_url_options
     { host: ENV["joincodebuddies.com"] || "localhost:3000" }
   end
